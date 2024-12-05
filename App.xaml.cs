945d75151208d03dba7129789f36ffa1f78ace4b
Denissabau03 <denisalexandru03@yahoo.com>
2024-12-05 18:47:49 +0200

    lab8 neterminat

--- App.xaml.cs
+++ App.xaml.cs
@@ -1,7 +1,24 @@
-﻿namespace SabauDenisLab7
+﻿using System;
+using SabauDenisLab7.Data;
+using System.IO;
+namespace SabauDenisLab7
 {
     public partial class App : Application
     {
+        static ShoppingListDatabase database;
+        public static ShoppingListDatabase Database
+        {
+            get
+            {
+                if (database == null)
+                {
+                    database = new
+                    ShoppingListDatabase(Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.
+                    LocalApplicationData), "ShoppingList.db3"));
+                }
+                return database;
+            }
+        }
         public App()
         {
             InitializeComponent();
